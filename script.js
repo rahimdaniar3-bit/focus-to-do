2153fc5a14a6908c2671cba8ed4911825361459c
rahimdaniar3-bit <rahimdaniar3@gmail.com>
2026-02-02 18:03:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let seconds = 0;
+let limit = 25 * 60; // 25 минут
 let total = 0;
 let interval = null;
 
@@ -19,6 +20,15 @@ document.getElementById("start").onclick = () => {
         `${String(h).padStart(2,"0")}:${String(m).padStart(2,"0")}:${String(s).padStart(2,"0")}`;
 
       totalEl.textContent = total;
+      if (seconds >= limit) {
+  clearInterval(interval);
+  interval = null;
+
+  alert(`Сессия ${currentSessionType} завершена!`);
+
+  saveSession(25); // сохраняем 25 минут
+  showStats();
+}
     }, 1000);
   }
 };
@@ -26,6 +36,9 @@ document.getElementById("start").onclick = () => {
 document.getElementById("stop").onclick = () => {
   clearInterval(interval);
   interval = null;
+
+  seconds = 0;
+  timer.textContent = "00:00:00";
 };
 // ===== ВЫБОР ТИПА СЕССИИ =====
 let currentSessionType = "Учёба";
@@ -39,3 +52,34 @@ document.getElementById("reset").onclick = () => {
   seconds = 0;
   timer.textContent = "00:00:00";
 };
+// ===== СОХРАНЕНИЕ СТАТИСТИКИ =====
+function saveSession(minutes) {
+  const today = new Date().toISOString().slice(0, 10);
+  let stats = JSON.parse(localStorage.getItem("stats") || "{}");
+
+  if (!stats[today]) stats[today] = {};
+  if (!stats[today][currentSessionType]) {
+    stats[today][currentSessionType] = 0;
+  }
+
+  stats[today][currentSessionType] += minutes;
+  localStorage.setItem("stats", JSON.stringify(stats));
+}
+function showStats() {
+  const statsEl = document.getElementById("stats");
+  const stats = JSON.parse(localStorage.getItem("stats") || "{}");
+
+  statsEl.innerHTML = "";
+
+  for (let day in stats) {
+    statsEl.innerHTML += `<b>${day}</b><br>`;
+    for (let type in stats[day]) {
+      statsEl.innerHTML += `${type}: ${stats[day][type]} мин<br>`;
+    }
+    statsEl.innerHTML += "<br>";
+  }
+
+  if (statsEl.innerHTML === "") {
+    statsEl.textContent = "Пока нет данных";
+  }
+}
